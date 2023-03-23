7345834cd8b4706529df6713e640150192fae527
MD NOUSHAD ANSARI <razamd260@gmail.com>
2023-03-23 17:44:46 +0530

    Project

---
+++ src/Loging/Parent.java
@@ -0,0 +1,13 @@
+package Loging;
+
+import org.testng.annotations.Test;
+
+public class Parent {
+	@Test
+	public void log()
+	{
+		LoginIntefaceConfirm p=new LoginIntefaceConfirm();
+		p.Logmodules();
+		
+	}
+}
